a4058c7d76aaa23e44951f0e5242cb58f8cacef5
Hirzi <64255651+HirziDevs@users.noreply.github.com>
2021-08-11 05:58:25 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const config = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+once('ready', () => {
+  console.log('Ready!');
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const data = []
+
+for (const file of commandFiles) {
+  const commandfile = require(`./commands/${file}`);
+  client.commands.set(commandfile.name, commandfile);
+  data.push({
+    name: commandfile.name,
+    description: commandfile.description,
+    options: commandfile.options
+  })
+}
+client.guilds.cache.get(config.guild)?.commands.set(data)
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+  if (!client.commands.has(interaction.commandName)) return;
+  try {
+    await client.commands.get(interaction.commandName).execute(interaction);
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true
+    });
+  }
+});
+
+client.login(config.token);
